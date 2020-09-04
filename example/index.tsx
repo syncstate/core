92afd213ac902091f7bffaaa481916ae1d9ba630
Himanshu Satija <himanshu219s@gmail.com>
2020-09-04 17:25:28 +0530

    Update example with new API and updated packages

--- example/index.tsx
+++ example/index.tsx
@@ -6,75 +6,55 @@ import { TodoApp } from './Todo';
 import { applyMiddleware } from 'redux';
 import history from '@syncstate/history';
 import { Provider, useDoc } from '@syncstate/react';
-import * as remote from '../src/remote';
+import * as remote from '@syncstate/remote-client';
 // @ts-ignore
 import socketIOClient from 'socket.io-client';
 
 const store = createDocStore({ todos: [], filter: 'all' }, [
-  // history.createInitializer(),
-  // // remote.createInitializer(),
-  // // history.createPlugin({ name: 'history' }),
-  // remote.createInitializer(),
+  history.createInitializer(),
+  remote.createInitializer(),
 ]);
 
-// store.dispatch(remote.enableRemote('/todos'));
+store.dispatch(remote.enableRemote('/todos'));
 
-// const socket = socketIOClient('http://localhost:3001', {
-//   timeout: 100000,
-// });
+const socket = socketIOClient('http://localhost:3001', {
+  timeout: 100000,
+});
 
-// store.observe('remote', '/paths/todos/loading', (loading, change) => {
-//   console.log('store.observe: remote status changed: ', loading);
-// });
+store.observe('remote', '/paths/::todos/loading', (loading, change) => {
+  console.log('store.observe: remote status changed: ', loading);
+});
 
-// remote.observeStatus(store, '/todos', loading => {
-//   console.log('remote.observeStatus: remote status changed: ', loading);
-// });
+remote.observeStatus(store, '/todos', loading => {
+  console.log('remote.observeStatus: remote status changed: ', loading);
+});
 
-// socket.emit('fetchDoc', '/todos');
-// store.dispatch(remote.setLoading('/todos', true));
-// console.log('Wewdwd', remote.getLoading(store, '/todos'));
+socket.emit('fetchDoc', '/todos');
+store.dispatch(remote.setLoading('/todos', true));
+console.log('Wewdwd', remote.getLoading(store, '/todos'));
 
-// socket.on('loaded', path => {
-//   store.dispatch(remote.setLoading(path, false));
-//   console.log('Wewdwd', remote.getLoading(store, path));
-// });
+socket.on('loaded', path => {
+  store.dispatch(remote.setLoading(path, false));
+  console.log('Wewdwd', remote.getLoading(store, path));
+});
 
-// socket.on('change', (path, patch) => {
-//   store.dispatch(remote.applyRemote(path, patch));
-// });
+socket.on('change', (path, patch) => {
+  store.dispatch(remote.applyRemote(path, patch));
+});
 
-// store.intercept(
-//   'doc',
-//   '/todos',
-//   (todos, change) => {
-//     if (!change.origin) {
-//       // Don't emit for patches received from server
-//       socket.emit('change', '/todos', change);
-//     }
-//     return change;
-//     // return null;
-//   },
-//   Infinity
-// );
-
-// remote.onLoadStart(store, "/todos", () => {
-//   console.log('started loading todos');
-// });
-
-// const [loading, setLoading] = useRootDoc(["remote", "todos/0", "loading"])
-
-// useRemoteStatus(["todos", 0])
-
-// store.rootObserve(["remote", "todos/0", "loading"])
-
-// remote.observeStatus(["todos", 0], () => {
-
-// })
-
-// remote.onLoadEnd(store, "/todos", () => {
-//   console.log('ended loading todos');
-// });
+store.intercept(
+  'doc',
+  '/todos',
+  (todos, change) => {
+    if (!change.origin) {
+      // Don't emit for patches received from server
+      socket.emit('change', '/todos', change);
+    }
+    return change;
+    // return null;
+  },
+  Infinity
+);
 
 const [doc, setDoc] = store.useSyncState('doc');
 setDoc(doc => (doc.test = 'paihwdih'));
